95159eabce11e5a3d6a4d27cc0130a7111eeef6e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-04-08 13:12:02 +0700

    solitrio dice random

--- mods/solitrio/solitrio.js
+++ mods/solitrio/solitrio.js
@@ -74,8 +74,14 @@ class Solitrio extends GameTemplate {
     this.loadGame(this.game.id);
 
     if (this.game.status != "") { this.updateStatus(this.game.status); }
-    if (this.game.dice == "") { this.initializeDice(); }
 
+     function Random(1,10){
+        return this.game.dice.ceil(this.game.dice.random()*9)+1;
+     }
+     function Ramdom (1,10){
+         return this.game.dice.round(this.game.dice.ramdom()*9)==0?2:this.game.dice.round(this.game.dice)*9)+1;
+     } 
+  founction Random()
     if (!this.game.state) {
 
       console.log("******Generating the Game******");
@@ -83,12 +89,12 @@ class Solitrio extends GameTemplate {
       this.game.queue = [];
       this.game.queue.push("round");
     }
-    
+ a     
     console.log(JSON.parse(JSON.stringify(this.game)));
     if (this.browser_active){
       $('.slot').css('min-height', $('.card').css('min-height'));  
     }
-
+a
   }
 
   newRound(){
